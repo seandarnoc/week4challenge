53041ed391a22e7f1e406efad2496a9eb0f0476a
Sean Conrad <sean.w.conrad@gmail.com>
2022-06-19 23:58:15 -0400

    added lines to index file and css file

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+
+
+
+document.getElementById("startQuiz").addEventListener("click",quizQuestions);
+
+//starts the quiz with the timer
+function quizQuestions() {
+    askQuestions();
+    clockID = setInterval(stopwatch, 1000);
+};
+
+function askQuestions() {
+
+}
