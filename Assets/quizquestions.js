8d9bd1e03f96f34903090089ffafb6b33fa8ccc7
Sean Conrad <sean.w.conrad@gmail.com>
2022-08-07 23:13:43 -0400

    added quiz questions

---
+++ Assets/quizquestions.js
@@ -0,0 +1,20 @@
+
+//quiz questions
+var questions = [
+    {
+        question: "What HTML tag is NOT included in the HEAD tag?",
+        choices: ["meta", "title", "base", "header"],
+        answer: "header"
+    },
+    {
+        question: "What are the two values that can be returned in a Boolean statement?",
+        choices: ["yes/no", "and/or", "if/then", "true/false"],
+        answer: "true/false"
+
+    },
+    {
+        question: "What does JS stand for?",
+        choices: ["javascript", "javasign", "javascripts", "jelloscript"],
+        answer: "javascript"
+    }
+]
